b782327fee89f88c0c4c4cd6f07086e4eeb1ea91
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-01-28 11:45:48 -0300

    add Repositories

---
+++ backend/src/main/java/repositories/DeliverRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslearnbds.entities.Deliver;
+
+public interface DeliverRepository extends JpaRepository<Deliver, Long>{
+
+}
---
+++ backend/src/main/java/repositories/OfferRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslearnbds.entities.Offer;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+}
